c59619adb1bd0e90576f38746d369802a0868548
Tropical Breeze RF <tropicalbreezecapetcleaning@gmail.com>
2026-03-23 15:43:08 -0400

    Ignore TypeScript errors in build

--- next.config.ts
+++ next.config.ts
@@ -2,4 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {};
 
+export default nextConfig;import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
 export default nextConfig;

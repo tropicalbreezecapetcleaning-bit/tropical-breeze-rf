3d6b9c2c2f332e212cc20cf6a5afb3dee18b6b14
Tropical Breeze RF <tropicalbreezecapetcleaning@gmail.com>
2026-04-11 15:56:43 -0400

    Fix booking page latin-1 encoding

---
+++ fix2.py
@@ -0,0 +1,4 @@
+﻿content = open("public/booking.html", "r", encoding="latin-1").read()
+content = content.encode("latin-1").decode("utf-8", errors="replace")
+open("public/booking.html", "w", encoding="utf-8").write(content)
+print("Done")
